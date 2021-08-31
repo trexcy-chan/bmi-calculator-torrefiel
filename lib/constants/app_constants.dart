2107553abbc937004c776485910c498e90e3b6fd
erenjaegerlangmalakas <trexcyfe.torrefiel@lsu.edu.ph>
2021-08-31 23:41:58 +0800

    First commit

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#FFFFFF");
+final Color accentHexColor = HexColor("#BD4B4B");
